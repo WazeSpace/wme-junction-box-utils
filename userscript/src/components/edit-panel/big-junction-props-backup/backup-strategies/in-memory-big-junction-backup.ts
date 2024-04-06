c5b9401b8569cb31bf9259ad37fe20ad3b2a5209
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-04-06 14:19:19 +0300

    feat: raise a warning to avoid uninteded save of a big junction deletion without snapshot restoration

--- userscript/src/components/edit-panel/big-junction-props-backup/backup-strategies/in-memory-big-junction-backup.ts
+++ userscript/src/components/edit-panel/big-junction-props-backup/backup-strategies/in-memory-big-junction-backup.ts
@@ -1,10 +1,30 @@
+import { SaveLock } from '@/utils/save-lock';
 import { BigJunctionBackupSnapshot } from '../backup-snapshot';
 import { BigJunctionBackupStrategy } from './big-junction-backup-strategy';
+import { WAS_RESTORED_METADATA_SYMBOL } from '../consts';
 
 export class InMemoryBigJunctionBackupStrategy
   implements BigJunctionBackupStrategy
 {
   static _storedSnapshot: BigJunctionBackupSnapshot = null;
+  static _unrestoredSnapshotSaveLockSet: boolean = false;
+
+  private static _setUnrestoreedSnapshotSaveLock() {
+    if (this._unrestoredSnapshotSaveLockSet) return;
+
+    SaveLock.addSoftLock(
+      {
+        code: 'jbu101',
+      },
+      () =>
+        this._storedSnapshot.createdFrom?.state === 'DELETE' &&
+        !Reflect.getMetadata(
+          WAS_RESTORED_METADATA_SYMBOL,
+          this._storedSnapshot,
+        ),
+    );
+    this._unrestoredSnapshotSaveLockSet = true;
+  }
 
   canSaveSnapshot(): boolean {
     return true;
@@ -12,6 +32,7 @@ export class InMemoryBigJunctionBackupStrategy
 
   saveSnapshot(snapshot: BigJunctionBackupSnapshot): void {
     InMemoryBigJunctionBackupStrategy._storedSnapshot = snapshot;
+    InMemoryBigJunctionBackupStrategy._setUnrestoreedSnapshotSaveLock();
   }
 
   getSnapshot(): BigJunctionBackupSnapshot {
