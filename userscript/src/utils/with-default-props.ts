0b19bd43b73541de47b8ab5a181cc50eb9587ef4
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-04-07 19:00:49 +0300

    feat: forward the reference object for withDefaultProps components

--- userscript/src/utils/with-default-props.ts
+++ userscript/src/utils/with-default-props.ts
@@ -1,26 +1,35 @@
 import {
   ComponentType,
   createElement,
-  FunctionComponent,
+  forwardRef,
+  ForwardRefExoticComponent,
+  ForwardRefRenderFunction,
   PropsWithChildren,
 } from 'react';
 
 export function withDefaultProps<P, DP extends Partial<P>>(
   component: ComponentType<PropsWithChildren<P>>,
   defaultProps: DP,
-): FunctionComponent<Omit<P, keyof DP> & Partial<DP>> {
-  const ComponentWithDefaultProps: FunctionComponent<
+): ForwardRefExoticComponent<Omit<P, keyof DP> & Partial<DP>> {
+  const ComponentWithDefaultProps: ForwardRefRenderFunction<
+    P extends { ref: infer U } ? U : never,
     Omit<P, keyof DP> & Partial<DP>
-  > = (props) => {
+  > = (props, ref) => {
     // Merge the provided props with the default props
     const mergedProps = { ...defaultProps, ...props } as P;
+    (mergedProps as any).ref = ref;
     // Render the original component with the merged props
     return createElement(component, mergedProps);
   };
 
   // Set display name for the new component
-  const componentName = component.displayName || component.name || 'Component';
+  const componentName =
+    component.displayName ||
+    component.name ||
+    (component as any).render?.displayName ||
+    (component as any).render?.name ||
+    'Component';
   ComponentWithDefaultProps.displayName = `WithDefaultProps(${componentName})`;
 
-  return ComponentWithDefaultProps;
+  return forwardRef(ComponentWithDefaultProps) as any;
 }
