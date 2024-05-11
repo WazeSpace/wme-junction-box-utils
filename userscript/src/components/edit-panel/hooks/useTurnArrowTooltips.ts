3bb91faa32dfdedbd19b355597dff49156976e28
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-05-11 15:10:25 +0300

    feat(JBU-12): add Roundabout Instruction Methods to the turn arrow tooltip

---
+++ userscript/src/components/edit-panel/hooks/useTurnArrowTooltips.ts
@@ -0,0 +1,42 @@
+import { useMutationObserver } from '@/hooks';
+import {
+  findFiberParentNode,
+  getReactFiberNode,
+  isReactProviderNode,
+} from '@/utils/react-fiber';
+import { useReducer } from 'react';
+
+type OpenTurnArrow = { element: HTMLDivElement; turnArrow: any };
+
+export function useTurnArrowTooltips(): ReadonlyArray<OpenTurnArrow> {
+  const [openTurnArrows, setOpenTurnArrowContainers] = useReducer(
+    (_prevState: OpenTurnArrow[], newTooltipContainers: HTMLDivElement[]) => {
+      return newTooltipContainers.map((tooltipContainer): OpenTurnArrow => {
+        const fiberNode = getReactFiberNode(tooltipContainer);
+        const turnArrowProviderNode = findFiberParentNode(
+          fiberNode,
+          (node) =>
+            isReactProviderNode(node) &&
+            typeof node.memoizedProps.value === 'object' &&
+            node.memoizedProps.value.hasOwnProperty('turnArrow'),
+        );
+        if (!isReactProviderNode(turnArrowProviderNode)) return null;
+        const turnArrow = turnArrowProviderNode.memoizedProps.value.turnArrow;
+        return { element: tooltipContainer, turnArrow };
+      });
+    },
+    [] as OpenTurnArrow[],
+  );
+  useMutationObserver(
+    document.body,
+    () => {
+      const turnArrowTooltips = document.body.querySelectorAll<HTMLDivElement>(
+        '& > [data-tippy-root] div.turn-arrow-tooltip',
+      );
+      setOpenTurnArrowContainers(Array.from(turnArrowTooltips));
+    },
+    { childList: true },
+  );
+
+  return openTurnArrows;
+}
--- userscript/src/components/edit-panel/roundabout-exit-instruction-normalization/RoundaboutExitInstructionNormalizationButton.tsx
+++ userscript/src/components/edit-panel/roundabout-exit-instruction-normalization/RoundaboutExitInstructionNormalizationButton.tsx
@@ -2,7 +2,18 @@ import { SegmentDataModel } from '@/@waze/Waze/DataModels/SegmentDataModel';
 import { useTranslate } from '@/hooks';
 import { RoundaboutInstructionEngine } from '@/roundabout-instruction-engine/roundabout-instruction-engine';
 import { WzButton } from '@wazespace/wme-react-components';
-import { useMemo } from 'react';
+import { cloneElement, useMemo } from 'react';
+import { useTurnArrowTooltips } from '../hooks/useTurnArrowTooltips';
+import { createPortal } from 'react-dom';
+import { TooltipControl } from './TooltipControl';
+import styled from '@emotion/styled';
+
+const TurnArrowTooltipButtonsRoot = styled('div')({
+  display: 'flex',
+  gap: '6px',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+});
 
 export interface Props {
   segment: SegmentDataModel;
@@ -21,16 +32,47 @@ export function RoundaboutExitInstructionNormalizationButton(props: Props) {
   const t = useTranslate();
   const nodeLabel = props.direction === 'forward' ? 'B' : 'A';
 
-  return engine.getPopulatedInstructionMethods().map((rim) => (
-    <WzButton
-      key={rim.type}
-      onClick={() => engine.applyInstructionMethod(rim)}
-      size="sm"
-      color="text"
-    >
-      {t(`jb_utils.segment.actions.exit_instructions.${rim.type}`, {
-        node: nodeLabel,
-      })}
-    </WzButton>
-  ));
+  const turnArrowTooltips = useTurnArrowTooltips();
+  const immediateTurnArrowTooltip = useMemo(
+    () =>
+      turnArrowTooltips.find(
+        (turnArrowTooltip) => !turnArrowTooltip.turnArrow.getTurn().isFarTurn(),
+      ),
+    [turnArrowTooltips],
+  );
+
+  const instructionMethodButtons = engine
+    .getPopulatedInstructionMethods()
+    .map((rim) => (
+      <WzButton
+        key={rim.type}
+        onClick={() => engine.applyInstructionMethod(rim)}
+        size="sm"
+        color="text"
+      >
+        {t(`jb_utils.segment.actions.exit_instructions.${rim.type}`, {
+          node: nodeLabel,
+        })}
+      </WzButton>
+    ));
+
+  const turnArrowPortal = immediateTurnArrowTooltip
+    ? createPortal(
+        <TooltipControl label={t('jb_utils.turn_tooltip.roundabout.title')}>
+          <TurnArrowTooltipButtonsRoot>
+            {instructionMethodButtons.map((button) =>
+              cloneElement(button, {
+                color: 'secondary',
+                children: t(
+                  `jb_utils.segment.actions.exit_instructions.${button.key}_SHORT`,
+                ),
+              }),
+            )}
+          </TurnArrowTooltipButtonsRoot>
+        </TooltipControl>,
+        immediateTurnArrowTooltip.element,
+      )
+    : null;
+
+  return [...instructionMethodButtons, turnArrowPortal];
 }
