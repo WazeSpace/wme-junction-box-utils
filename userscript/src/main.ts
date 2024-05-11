cf656d9ccdc047af92b6c54602e516f7809ef05e
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-05-11 15:58:04 +0300

    chore: install axios and set the adapter to gmxhr

--- userscript/src/main.ts
+++ userscript/src/main.ts
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import { createElement } from 'react';
 import { App } from './App';
 import { migratePreferencesFromRoundaboutJB } from './legacy-migrator';
+import axios from 'axios';
+import { axiosGmXhrAdapter } from './tampermonkey/axios-gmxhr-adapter';
 
 export default async function bootstrap() {
+  axios.defaults.adapter = axiosGmXhrAdapter;
   migratePreferencesFromRoundaboutJB();
   ReactDOM.render(createElement(App), document.createElement('div'));
 }
