2c9ca43a0c2edb162af15e83e128333cde11bba3
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-05-25 11:44:23 +0300

    fix: comp. issues with picking the right component

--- userscript/src/components/edit-panel/roundabout-exit-instruction-normalization/TooltipControl.tsx
+++ userscript/src/components/edit-panel/roundabout-exit-instruction-normalization/TooltipControl.tsx
@@ -1,3 +1,5 @@
+import { useVersionDependantConfig } from '@/hooks';
+import { WzLabel } from '@wazespace/wme-react-components';
 import { ReactNode } from 'react';
 
 interface TooltipControlProps {
@@ -5,9 +7,13 @@ interface TooltipControlProps {
   children: ReactNode;
 }
 export function TooltipControl({ label, children }: TooltipControlProps) {
+  const { LabelComponent } = useVersionDependantConfig([
+    { v: null, LabelComponent: 'label' },
+    { v: { M: 2, m: 229 }, LabelComponent: WzLabel },
+  ]);
   return (
     <div className="tooltip-control">
-      <label>{label}</label>
+      <LabelComponent>{label}</LabelComponent>
       {children}
     </div>
   );
