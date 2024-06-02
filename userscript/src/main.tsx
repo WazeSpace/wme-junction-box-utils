b87c82242fbcf11dda60f20f290e5422b187083b
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-06-02 21:26:39 +0300

    feat: use fallback translations if remote translations are missing

--- userscript/src/main.tsx
+++ userscript/src/main.tsx
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import deepmerge from 'deepmerge';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { migratePreferencesFromRoundaboutJB } from './legacy-migrator';
@@ -7,6 +8,7 @@ import { axiosGmXhrAdapter } from './tampermonkey/axios-gmxhr-adapter';
 import { getWazeMapEditorWindow } from './utils/get-wme-window';
 import CrowdinOtaClient from './localization/crowdin-ota/ota-client';
 import { addGoogleTagManagerToPage } from './google-analytics';
+import fallbackTranslations from './resources/localization/userscript.json';
 
 export default async function bootstrap() {
   axios.defaults.adapter = axiosGmXhrAdapter;
@@ -15,11 +17,12 @@ export default async function bootstrap() {
   const translations = await new CrowdinOtaClient(
     process.env.CROWDIN_DISTRIBUTION_HASH,
   ).getStringsByLocale(currentLocale);
+  const mergedTranslations = deepmerge(fallbackTranslations, translations);
 
   addGoogleTagManagerToPage(process.env.G_MEASUREMENT_ID);
   migratePreferencesFromRoundaboutJB();
   ReactDOM.render(
-    <App translations={{ [currentLocale]: translations }} />,
+    <App translations={{ [currentLocale]: mergedTranslations }} />,
     document.createElement('div'),
   );
 }
