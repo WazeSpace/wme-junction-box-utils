9994d741a6b892f3b3e26a5ed27a0b79855033af
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-06-03 19:16:27 +0300

    fix: incorrect import of turn instruction method

--- userscript/src/instruction-application-engine/methods/denormalization-method.ts
+++ userscript/src/instruction-application-engine/methods/denormalization-method.ts
@@ -1,6 +1,6 @@
 import { Turn } from '@/@waze/Waze/Model/turn';
 import { TurnInstructionOpcode } from '@/@waze/Waze/Model/turn-instruction-opcode.enum';
-import { TurnInstructionMethod } from '@/roundabout-instruction-engine/methods/turn-instruction-method';
+import { TurnInstructionMethod } from './turn-instruction-method';
 
 const deNormalizationMethod: TurnInstructionMethod = {
   type: 'DE_NORMALIZATION',
