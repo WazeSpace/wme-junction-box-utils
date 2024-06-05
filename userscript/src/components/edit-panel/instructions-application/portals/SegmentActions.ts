592da922c9b46d4e58da007fdb7cdd90f942c522
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-06-05 09:06:32 +0300

    fix(JBU-59): improperly resolved portal container for segment actions

--- userscript/src/components/edit-panel/instructions-application/portals/SegmentActions.ts
+++ userscript/src/components/edit-panel/instructions-application/portals/SegmentActions.ts
@@ -1,7 +1,10 @@
 import { createReactPortal } from '@/utils';
 
 export const SegmentActionsPortal = createReactPortal(() =>
-  document.querySelector(
-    '.segment-edit-section #segment-edit-general div:has(+ form)',
-  ),
+  [
+    document.querySelector('#segment-edit-general .form-group.more-actions'),
+    document.querySelector(
+      '.segment-edit-section #segment-edit-general div:has(+ form)',
+    ),
+  ].find(Boolean),
 );
