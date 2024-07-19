c744c8b8ecbfa47bc34520eab41fa8979d063181
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-07-20 00:13:04 +0300

    fix: sync conflicts with SetTurnArrowsButton caused the feat to not work

--- userscript/src/components/edit-panel/lg-auto-turn-arrow/components/SetTurnArrowsButton.tsx
+++ userscript/src/components/edit-panel/lg-auto-turn-arrow/components/SetTurnArrowsButton.tsx
@@ -1,9 +1,10 @@
 import { usePreference, useTranslate } from '@/hooks';
+import { Logger } from '@/logger';
 import { WzButton } from '@wazespace/wme-react-components';
 import { createPortal } from 'react-dom';
 import { useLaneGuidanceControl, useFarTurnsControlLabel } from '../hooks';
 import { setControlTurnArrows } from '../utils';
-import { SyntheticEvent, useEffect, useRef } from 'react';
+import { SyntheticEvent, useCallback } from 'react';
 import { SetTurnArrowsEvent } from '../events';
 
 interface SetTurnArrowsButtonProps {
@@ -19,30 +20,27 @@ export function SetTurnArrowsButton({
   );
   const laneGuidanceControl = useLaneGuidanceControl(lanesDirection);
   const controlLabel = useFarTurnsControlLabel(lanesDirection);
-  const buttonRef = useRef<HTMLButtonElement>(null);
+  const buttonRef = useCallback(
+    (button: HTMLButtonElement) => {
+      if (autoSetTurnArrows && button && laneGuidanceControl) {
+        Logger.log('setting turn arrows');
+        const event = new SetTurnArrowsEvent({
+          associatedButton: button,
+        });
+        onTurnArrowsSetAutomatically?.(event);
+        if (!event.defaultPrevented) setControlTurnArrows(laneGuidanceControl);
+      }
+    },
+    [autoSetTurnArrows, laneGuidanceControl, onTurnArrowsSetAutomatically],
+  );
   const t = useTranslate('jb_utils.lanes');
-
-  useEffect(() => {
-    if (autoSetTurnArrows && controlLabel && buttonRef.current) {
-      const event = new SetTurnArrowsEvent({
-        associatedButton: buttonRef.current,
-      });
-      onTurnArrowsSetAutomatically?.(event);
-      if (!event.defaultPrevented) setControlTurnArrows(laneGuidanceControl);
-    }
-  }, [
-    autoSetTurnArrows,
-    controlLabel,
-    laneGuidanceControl,
-    onTurnArrowsSetAutomatically,
-  ]);
 
   const setTurnArrows = (e: SyntheticEvent) => {
     setControlTurnArrows(laneGuidanceControl);
     if (e.currentTarget instanceof HTMLElement) e.currentTarget.blur();
   };
 
-  if (!controlLabel) return null;
+  if (!controlLabel || !laneGuidanceControl) return null;
   return createPortal(
     <WzButton
       style={{ verticalAlign: 'middle', marginLeft: 8 }}
