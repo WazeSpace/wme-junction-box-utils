e77b8c46ff7af16cbc3940668f6d3079bcd96046
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-10-15 01:00:57 +0300

    fix(JBU-63): DrawControl retrieval is unavailable, to underlying changes
    Encountered on WME FE v2.252-15 by WME user hellror
    
    Reason: Underlying changes done by the WME team to the way of storing the DrawControls in React's tree (useMemo instead of useRef). More info in ticket.
    
    Fix: Allow retrieval of DrawControl from both the useMemo hook and useRef hook, via additional utility function that tries both, and returns the first who successfully passed the object shape requirements. This fix should be effective even if WME team later switches back to the `useRef` approach, which would work because of the implementation which tries to extract info from both `useRef` and `useMemo`.
    
    For future reference, if the issue occurs again, check where the DrawControl is stored, if necessary create new utility functions to test against different types of raw memoizedState shapes, and update the `findInfoInRawState` function to extract information from the new shapes.

--- userscript/src/utils/wme-feature-creation/big-junction/draw-callback-impl/drawer.ts
+++ userscript/src/utils/wme-feature-creation/big-junction/draw-callback-impl/drawer.ts
@@ -32,22 +32,46 @@ function isRefObject<T>(
   );
 }
 
+function isMemoHook<T>(
+  rawState: any,
+  assertion: (value: any) => boolean,
+): rawState is [T, any] {
+  return (
+    Array.isArray(rawState) &&
+    rawState.length === 2 &&
+    Array.isArray(rawState[1]) &&
+    assertion(rawState[0])
+  );
+}
+
+function findInfoInRawState<T>(
+  rawState: any,
+  assertion: (value: any) => boolean,
+): [true, T] | [false, null] {
+  if (isRefObject<T>(rawState, assertion)) {
+    return [true, rawState.current];
+  }
+
+  if (isMemoHook<T>(rawState, assertion)) {
+    return [true, rawState[0]];
+  }
+
+  return [false, null];
+}
+
 function findDrawControlInFiberNode(fiberNode: Fiber) {
   let currentStateRef = fiberNode.memoizedState;
   while (currentStateRef) {
     const currentState = currentStateRef.memoizedState;
-    if (
-      isRefObject<{ drawCallback: any }>(
-        currentState,
-        (refValue) =>
-          typeof refValue === 'object' &&
-          refValue.__proto__.hasOwnProperty('CLASS_NAME') &&
-          refValue.__proto__.CLASS_NAME === 'Waze.Control.DrawFeature' &&
-          typeof refValue.drawCallback === 'function',
-      )
-    ) {
-      return currentState.current;
-    }
+    const [success, value] = findInfoInRawState<{ drawCallback: any }>(
+      currentState,
+      (value) =>
+        typeof value === 'object' &&
+        value.__proto__.hasOwnProperty('CLASS_NAME') &&
+        value.__proto__.CLASS_NAME === 'Waze.Control.DrawFeature' &&
+        typeof value.drawCallback === 'function',
+    );
+    if (success) return value;
 
     currentStateRef = currentStateRef.next;
   }
