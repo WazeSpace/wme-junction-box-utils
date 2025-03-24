1cfc7604172d174709fd11eaf34b6641148ffcef
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-03-24 21:57:47 +0200

    feat: temporarily disable roundabout geometry recreation due to Waze changes

--- userscript/src/components/edit-panel/roundabout-perimeter-geometry/CloneRoundaboutGeometryToBigJunctionButton.tsx
+++ userscript/src/components/edit-panel/roundabout-perimeter-geometry/CloneRoundaboutGeometryToBigJunctionButton.tsx
@@ -1,6 +1,7 @@
 import { AddBigJunctionAction, MultiAction } from '@/@waze/Waze/actions';
 import { BigJunctionDataModel } from '@/@waze/Waze/DataModels/BigJunctionDataModel';
 import { UpdateBigJunctionGeometryToRoundaboutAction } from '@/actions';
+import { ConditionalTooltip } from '@/components/ConditionalTooltip';
 import { BigJunctionBackup } from '@/components/edit-panel/big-junction-backup';
 import { RestoreBigJunctionBackupAction } from '@/components/edit-panel/big-junction-backup/actions';
 import { gtag } from '@/google-analytics';
@@ -32,32 +33,36 @@ export function CloneRoundaboutGeometryToBigJunctionButton({
     );
   };
 
-  const recreateBigJunction = () => {
-    const backup = BigJunctionBackup.fromBigJunction(bigJunction);
-    const newBigJunctionAction = createAddBigJunctionAction(
-      bigJunction.getAttribute('geoJSONGeometry'),
-    );
-    newBigJunctionAction.__jbuSkipAutoRoundaboutize = true;
-    const updateBigJunctionGeomAction =
-      getUpdateBigJunctionGeometryAction(newBigJunctionAction);
-    const multiAction = new MultiAction([
-      createDeleteBigJunctionAction(bigJunction),
-      newBigJunctionAction,
-      updateBigJunctionGeomAction,
-      new RestoreBigJunctionBackupAction(
-        newBigJunctionAction.bigJunction,
-        backup,
-        [],
-      ),
-    ]);
-    multiAction.generateDescription = () => {
-      updateBigJunctionGeomAction.generateDescription();
-      (multiAction as any)._description = (
-        updateBigJunctionGeomAction as any
-      )._description;
-    };
-    return multiAction;
-  };
+  //! Temporary disabled
+  //  The code relies on the delete button to have a deleteFeatures method somewhere in the stateNode
+  //  Unfortunately, Waze has changed something, and the delete button no longer has this method
+  const recreateBigJunction = null;
+  // const recreateBigJunction = () => {
+  //   const backup = BigJunctionBackup.fromBigJunction(bigJunction);
+  //   const newBigJunctionAction = createAddBigJunctionAction(
+  //     bigJunction.getAttribute('geoJSONGeometry'),
+  //   );
+  //   newBigJunctionAction.__jbuSkipAutoRoundaboutize = true;
+  //   const updateBigJunctionGeomAction =
+  //     getUpdateBigJunctionGeometryAction(newBigJunctionAction);
+  //   const multiAction = new MultiAction([
+  //     createDeleteBigJunctionAction(bigJunction),
+  //     newBigJunctionAction,
+  //     updateBigJunctionGeomAction,
+  //     new RestoreBigJunctionBackupAction(
+  //       newBigJunctionAction.bigJunction,
+  //       backup,
+  //       [],
+  //     ),
+  //   ]);
+  //   multiAction.generateDescription = () => {
+  //     updateBigJunctionGeomAction.generateDescription();
+  //     (multiAction as any)._description = (
+  //       updateBigJunctionGeomAction as any
+  //     )._description;
+  //   };
+  //   return multiAction;
+  // };
 
   const handleButtonClick = () => {
     const actionToApply = addBigJunctionAction
@@ -67,9 +72,47 @@ export function CloneRoundaboutGeometryToBigJunctionButton({
     gtag('event', 'roundaboutize_big_junction');
   };
 
-  return (
-    <WzButton color="text" onClick={handleButtonClick}>
+  const buttonContent = (
+    <WzButton
+      color="text"
+      onClick={handleButtonClick}
+      disabled={!addBigJunctionAction && !recreateBigJunction}
+    >
       {t('jb_utils.big_junction.actions.clone_roundabout_geom')}
     </WzButton>
   );
+
+  if (!addBigJunctionAction && !recreateBigJunction) {
+    return (
+      <wz-rich-tooltip>
+        <div></div>
+        <wz-tooltip-source>
+          {buttonContent}
+          <wz-tooltip-target />
+        </wz-tooltip-source>
+        <wz-tooltip-content>
+          <div>
+            <wz-subhead1>Roundaboutization unavailable</wz-subhead1>
+            <wz-body2
+              style={{ display: 'block', marginTop: 16, lineHeight: 1.3 }}
+            >
+              Roundabouzation is temporary unavailable for existing junction
+              boxes.
+              <br />
+              As a workaround, backup the properties and manually re-create the
+              junction box.
+            </wz-body2>
+            <wz-caption
+              style={{ display: 'block', marginTop: 8, lineHeight: 1.3 }}
+            >
+              We're working hard to bring back this option, however, it might
+              take a while before we can do so.
+            </wz-caption>
+          </div>
+        </wz-tooltip-content>
+      </wz-rich-tooltip>
+    );
+  }
+
+  return buttonContent;
 }
